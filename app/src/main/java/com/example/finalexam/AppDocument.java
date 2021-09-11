7e2421b8d7c8dc1445d01b9df6d7c425f3dbacd7
Niloy <niloysaha173@gmail.com>
2021-09-11 23:06:21 +0600

    New java file added and a textview added in BaseActivity

--- app/src/main/java/com/example/finalexam/AppDocument.java
+++ app/src/main/java/com/example/finalexam/AppDocument.java
@@ -3,12 +3,23 @@ package com.example.finalexam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class AppDocument extends AppCompatActivity {
 
+    WebView document;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_document);
+        document = findViewById(R.id.webView);
+
+        String htmlAsString = getString(R.string.html);
+        //Spanned htmlAsSpanned = Html.fromHtml(htmlAsString);
+
+        document.loadDataWithBaseURL(null,htmlAsString,"text/html", "utf-8", null);
     }
 }
